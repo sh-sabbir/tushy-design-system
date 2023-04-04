cba85bb9acf912f43d0cd56eb555ac6b5aa077e8
Maya-tusy <129828513+Maya-tusy@users.noreply.github.com>
2023-04-04 17:38:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ import "./components.scss";
 import { Marquee } from "./js/marquee.js";
 
 window.addEventListener("load", () => {
-	Marquee(".tushy-anim-banner", 0.2);
+	//Marquee(".tushy-anim-banner", 0.2);
 });
